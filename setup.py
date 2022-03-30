70cbca402270a9c2354a2fd27e9fd69d12bbf29f
Alex Ruddick <a.ruddick@numat-tech.com>
2022-03-30 14:59:47 -0500

    Drop Python3.6 and support 3.10

--- setup.py
+++ setup.py
@@ -4,15 +4,15 @@ from setuptools import setup
 
 if version_info < (3, 6):
     raise ImportError("This module requires Python >=3.6 for asyncio support")
-if version_info >= (3, 10):
-    raise ImportError("This module depends on pymodbus, which is incompatible with Python 3.10")
+if version_info < (3, 7):
+    raise ImportError("This module requires Python >=3.7.  Use 0.6.0 for Python3.6")
 
 with open('README.md', 'r') as in_file:
     long_description = in_file.read()
 
 setup(
     name='productivity',
-    version='0.6.0',
+    version='0.7.0',
     description="Python driver for AutomationDirect Productivity Series PLCs.",
     long_description=long_description,
     long_description_content_type='text/markdown',
@@ -41,10 +41,10 @@ setup(
         'Natural Language :: English',
         'Programming Language :: Python',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
         'Topic :: Scientific/Engineering :: Human Machine Interfaces'
     ]
 )
