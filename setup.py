cd4f6070e44dff6deb4bc8b8a51bca5f959c90e3
Alex Ruddick <a.ruddick@numat-tech.com>
2023-03-06 11:24:53 -0600

    use pre-release pymodbus-3.2

--- setup.py
+++ setup.py
@@ -22,10 +22,7 @@ setup(
         'console_scripts': [('productivity = productivity:command_line')]
     },
     install_requires=[
-        'pymodbus>=2.4.0,<3; python_version == "3.7"',
-        'pymodbus>=2.4.0; python_version == "3.8"',
-        'pymodbus>=2.4.0; python_version == "3.9"',
-        'pymodbus>=3.0.2,<3.3.0; python_version >= "3.10"',
+        'pymodbus @ git+ssh://git@github.com/pymodbus-dev/pymodbus@97ffffc9e7dc164161dae936782c49d7dae8a7de#egg=pymodbus',
         'PyYAML',
     ],
     extras_require={
