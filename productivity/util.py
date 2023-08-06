7c20f41b1f9afd8d4188e982abc0a9855bc74980
Paul Boone <paul.boone@numat-tech.com>
2023-08-06 17:59:38 -0400

    try func instead of LOg

--- productivity/util.py
+++ productivity/util.py
@@ -8,8 +8,8 @@ import logging
 
 try:
     import pymodbus
-    pymodbus.logging.Log.apply_logging_config(logging.INFO, None)
-    # pymodbus.logging.pymodbus_apply_logging_config()
+    # pymodbus.logging.Log.apply_logging_config(logging.INFO, None)
+    pymodbus.logging.pymodbus_apply_logging_config("INFO")
     from pymodbus.client import AsyncModbusTcpClient  # 3.x
 except ImportError:  # 2.4.x - 2.5.x
     from pymodbus.client.asynchronous.async_io import ReconnectingAsyncioModbusTcpClient  # type: ignore
