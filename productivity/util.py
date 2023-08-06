2d9eef6254824c73135ec53e53c7e44d33cc6444
Paul Boone <paul.boone@numat-tech.com>
2023-08-06 17:39:14 -0400

    enable pymodbus info logging

--- productivity/util.py
+++ productivity/util.py
@@ -8,7 +8,7 @@ import logging
 
 try:
     import pymodbus
-
+    pymodbus.logging.Log.apply_logging_config(logging.INFO, None)
     # pymodbus.logging.pymodbus_apply_logging_config()
     from pymodbus.client import AsyncModbusTcpClient  # 3.x
 except ImportError:  # 2.4.x - 2.5.x
